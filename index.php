2d7d060e4c2f658c8a309333fc55f2b71ef79fc8
maybecher <cfalcao.study@gmail.com>
2026-09-04 09:44:21 -0300

    criação do projeto inicial em PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$nome = "Caroline Falcão de Souza";
+$profissao = "Instrutora de Formação Profissional";
+$mensagem = "Meu primeiro projeto em PHP com Git e GitHub";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Projeto PHP - Carol Souza</title>
+</head>
+<body>
+
+    <h1><?php echo $nome; ?></h1>
+
+    <h2><?php echo $profissao; ?></h2>
+
+    <p><?php echo $mensagem; ?></p>
+
+</body>
+</html>
